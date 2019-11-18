6db86ec9b8f2f2e46039339ef428004f881178b2
NaeemMeman <naeemmeman@gmail.com>
2019-11-17 20:46:58 -0500

    added shell

---
+++ src/shell.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include<string.h> 
+#include "tree.h"
+//#include "list.h"
+#include "path.h"
+//#include "exit.h"
+
+#define clear() printf("\033[H\033[J") 
+
+int main() {
+	//initialize the shell
+	clear();
+	char username[100];
+	printf("Enter username: ");
+	scanf("%s", username);
+	sleep(1);
+	clear();
+	
+	//list of commands
+	char* ListOfOwnCmds[4];
+	ListOfOwnCmds[0] = "tree"; 
+    ListOfOwnCmds[1] = "list"; 
+    ListOfOwnCmds[2] = "path"; 
+    ListOfOwnCmds[3] = "exit"; 
+	
+	
+	while(1) {
+		//get curent working directory
+		char cwd[1000];
+		getcwd(cwd, sizeof(cwd));
+		
+		//get the command from the user
+		char buf[100]; 
+		printf("\n%s@%s>> ", username, cwd);
+		scanf("%s", buf);
+				
+		//if a command is entered save it to history for exit function
+		if (strlen(buf) != 0) { 
+			//add to history 
+		} 
+		
+		//math the command with the one that the user entered
+		int cmd = 4;
+		for(int i=0; i<4; i++) {
+			if(strcmp(ListOfOwnCmds[i],buf) == 0) {
+				cmd = i;
+				break;
+			}
+		}
+		
+		//choose the command to run
+		switch(cmd) {
+			case 0:
+				tree();
+				break;
+			case 1:
+				printf("temp");
+				break;
+			case 2:
+				path();
+				break;
+			case 3:
+				printf("temp");
+				break;
+			case 4:
+				printf("Use a valid command");
+				break;
+		}
+	}
+	
+
+	return 0;
+}
